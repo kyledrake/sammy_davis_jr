752eae2fcf04bf636ba0e87f1e7bdb40c55016cb
Kyle Drake <kyledrake@gmail.com>
2010-07-18 15:21:55 -0700

    Pull old fixture example

--- tests/fixtures.rb
+++ tests/fixtures.rb
@@ -1,9 +1,3 @@
 # Crazy moon code? Read these!
 # http://github.com/datamapper/dm-more/tree/master/dm-sweatshop/
 # http://github.com/benburkert/randexp/
-
-=begin
-User.fixture {{
-  :fb_user_id => FB_PARAMS['fb_sig_user']
-}}
-=end
